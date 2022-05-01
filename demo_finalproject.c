c2fcec1daec90eadf142a0d8b897635d605f2b2c
ryan prana <97663926+prana8@users.noreply.github.com>
2022-05-02 01:05:48 +0800

    Create demo_finalproject.c

---
+++ demo_finalproject.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+struct List
+{
+    int saldo;
+};
+void penyimpanan(int n, List uang){
+    printf("\n\n========================\n");
+    printf("Masukan Jublah Uang : ");
+    scanf("%d", &uang.saldo);
+    printf("Uang berhasil tersimpan!!!\n");
+    printf("Jumblah Saldo : %d\n", uang.saldo);
+
+};
+
+
+
+
+int main(){
+    int bahasa, menu, n;
+    List uang;
+    
+    printf("========================\n");
+    printf("=     Program Atm      =\n");
+    printf("========================\n");
+
+    printf("Selamat Datang Di Bank Kami\n");
+    printf("Pilih Bahasa\n");
+    printf("[1]. INDONESIA\n");
+    printf("[2]. ENGLISH\n");
+
+    printf("Masukan Pilihan ([1]/[2]) : ");
+    scanf("%d", &bahasa);
+
+    switch (bahasa)
+    {
+    case 1:
+    system("cls");
+    printf("========================\n");
+    printf("=     Program ATM      =\n");
+    printf("========================\n");
+
+    printf("Menu : \n");
+    printf("1. Cek Saldo\n");
+    printf("2. Penyimpanan\n");
+    printf("3. Penarikan\n");
+    printf("4. Transfer\n");
+    printf("Masukan Pilihan Menu : ");
+    scanf("%d", &menu);
+    if (menu==2)
+    {
+        penyimpanan(n, uang);
+    }
+    
+
+        break;
+    case 2 :
+    system("cls");
+    printf("========================\n");
+    printf("=     ATM Program      =\n");
+    printf("========================\n");
+
+    printf("Menu : \n");
+    printf("1. Inquiry\n");
+    printf("1. Top Up\n");
+    printf("2. Withdrawal\n");
+    printf("3. Transfer\n");
+
+    printf("Input The Menu : ");
+    scanf("%d",&menu);
+
+    default:
+        break;
+    
+    }
+}
