ad694c0e64fed41139ca268a44e326df061309d9
ryan prana <97663926+prana8@users.noreply.github.com>
2022-05-03 00:50:04 +0800

    Update demo_finalproject.c

--- demo_finalproject.c
+++ demo_finalproject.c
@@ -3,30 +3,42 @@
 #include <time.h>
 #include <string.h>
 
-struct List
+struct rekening
 {
     int saldo;
 };
-void penyimpanan(int n, List uang){
-    printf("Masukan Jublah Uang : Rp. ");
-    scanf("%d", &uang.saldo);
-    printf("Uang berhasil tersimpan!!!\n");
-    printf("Jumblah Saldo : %d\n", uang.saldo);
 
-};
-
-
-
+void cek_saldo_indo(rekening rek){
+    time_t waktu;
+    time (&waktu);
+    system("cls");
+    printf(ctime(&waktu));
+    printf("=========================\n");
+    printf("=      Program Atm      =\n");
+    printf("=========================\n");
+    printf("   INFORMASI SALDO\n\n");
+    printf("SALDO : Rp.%d\n\n", rek.saldo);
+}
+void cek_saldo_ing(rekening rek){
+    time_t waktu;
+    time (&waktu);
+    system("cls");
+    printf(ctime(&waktu));
+    printf("=========================\n");
+    printf("=      Program Atm      =\n");
+    printf("=========================\n");
+    printf("   INQUIRY INFORMATION\n\n");
+    printf("BALANCE : Rp.%d\n\n", rek.saldo);
+}
 
 int main(){
-    int bahasa, menu, n;
-    List uang;
+    int bahasa, menu, n = 0,  i, setor;
+    rekening rek;
     char yn;
     
-    printf("========================\n");
-    printf("=     Program Atm      =\n");
-    printf("========================\n");
-
+    printf("=========================\n");
+    printf("=      Program Atm      =\n");
+    printf("=========================\n");
     printf("Selamat Datang Di Bank Kami\n");
     printf("Pilih Bahasa\n");
     printf("[1]. INDONESIA\n");
@@ -40,59 +52,121 @@ int main(){
     case 1:
     awal:
     system("cls");
-    printf("========================\n");
-    printf("=     Program ATM      =\n");
-    printf("========================\n");
+    printf("=========================\n");
+    printf("=      Program ATM      =\n");
+    printf("=========================\n");
 
     printf("Menu : \n");
     printf("1. Cek Saldo\n");
     printf("2. Penyimpanan\n");
     printf("3. Penarikan\n");
     printf("4. Transfer\n");
+    printf("5. Selesai\n");
     printf("Masukan Pilihan Menu : ");
     scanf("%d", &menu);
-    if (menu==2)
-    {
-        penyimpanan(n, uang);
-        lanjut:
+    if (menu==1){
+        cek_saldo_indo(rek);
+        lanjut1:
+        fflush(stdin);
         printf("Lanjutkan Transaksi?? (y/n) : ");
         scanf("%c", &yn);
-        if ((yn == 'y' || yn == 'Y'))
-        {
+        if ((yn == 'y' || yn == 'Y')){
             goto awal;
-        }
-        else if ((yn == 'n' || yn == 'N'))
-        {
+        } else if ((yn == 'n' || yn == 'N')){
             break;
+        }else{
+            goto lanjut1;
         }
-        else
-        {
-            goto lanjut;
-        }
-        
-        
         
     }
-    
+    else if (menu==2)
+    {
+        system("cls");
+        printf("=========================\n");
+        printf("=      Program ATM      =\n");
+        printf("=========================\n\n");
+        printf("Masukan uang : Rp. ");
+        scanf("%d", &setor);
+        rek.saldo = rek.saldo + setor;
+        printf("Uang Berhasil Tersimpan!!\n");
+        printf("________________________\n");
+        printf("Total Saldo : %d\n", rek.saldo);
+        lanjut2:
+        fflush(stdin);
+        printf("Lanjutkan Transaksi?? (y/n) : ");
+        scanf("%c", &yn);
+        if ((yn == 'y' || yn == 'Y')){
+            goto awal;
 
+        } else if ((yn == 'N' || yn == 'n')){
+            break;
+        } else{
+            goto lanjut2;
+        }
+        
+    }
         break;
     case 2 :
+    awal1:
     system("cls");
-    printf("========================\n");
-    printf("=     ATM Program      =\n");
-    printf("========================\n");
+    printf("=========================\n");
+    printf("=      ATM Program      =\n");
+    printf("=========================\n");
 
     printf("Menu : \n");
-    printf("1. Inquiry\n");
-    printf("1. Top Up\n");
-    printf("2. Withdrawal\n");
-    printf("3. Transfer\n");
+    printf("1. Balance Inquiry\n");
+    printf("2. Top Up\n");
+    printf("3. Withdrawal\n");
+    printf("4. Transfer\n");
+    printf("5. Exit\n");
 
     printf("Input The Menu : ");
     scanf("%d",&menu);
+     if (menu==1){
+        cek_saldo_ing(rek);
+        lanjut3:
+        fflush(stdin);
+        printf("continue transaction?? (y/n) : ");
+        scanf("%c", &yn);
+        if ((yn == 'y' || yn == 'Y')){
+            goto awal1;
+        } else if ((yn == 'n' || yn == 'N')){
+            break;
+        }else{
+            goto lanjut3;
+        }
+        
+    }
+    else if (menu==2)
+    {
+        system("cls");
+        printf("=========================\n");
+        printf("=      Program ATM      =\n");
+        printf("=========================\n\n");
+        printf("Enter Amount : Rp. ");
+        scanf("%d", &setor);
+        rek.saldo = rek.saldo + setor;
+        printf("done!!\n");
+        printf("________________________\n");
+        printf("Total Balance : %d\n", rek.saldo);
+        lanjut4:
+        fflush(stdin);
+        printf("continue transaction (y/n) : ");
+        scanf("%c", &yn);
+        if ((yn == 'y' || yn == 'Y')){
+            goto awal;
+
+        } else if ((yn == 'N' || yn == 'n')){
+            break;
+        } else{
+            goto lanjut4;
+        }
+        
+    }
 
     default:
         break;
     
     }
+    return 0;
 }
